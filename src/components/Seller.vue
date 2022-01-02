c9cc259dda8142eee8739a589c38347fa443dfe2
dlxorb96 <xorb_1@naver.com>
2022-01-02 19:19:33 +0900

    2022.01.02
    물품 일괄 삭제 기능 추가

--- src/components/Seller.vue
+++ src/components/Seller.vue
@@ -7,22 +7,32 @@
         물품등록
       </el-button>
       <router-link to="ItemInsertBatch">
-          <el-button type="success" style="margin-left : 10px" size="small" @click="insertItem">
-            물품일괄등록  
-            </el-button>
+        <el-button type="success" style="margin-left : 10px" size="small" @click="insertItem">
+          물품일괄등록  
+        </el-button>
       </router-link>
+      <el-button type="danger" style="margin-left : 10px" size="small" @click="deleteSomeItem">
+          물품일괄삭제  
+      </el-button>
 
-     
+     {{items}}
         
 
-        <el-table :data="items" size="mini" style="width: 100%;">
-            <el-table-column label="코드" width="180">
-                <template #default="scope">
-                    <div @click="rowClick(scope)" style="cursor:pointer;">
-                        {{scope.$index+1}}
-                    </div>
-                </template>
-            </el-table-column>    
+      <el-table :data="items" size="mini" style="width: 100%;">
+
+       <el-table-column label="선택" width="50">
+        <template #default="scope">
+          <el-checkbox v-model="scope.row.chk" @change="clickChk(scope.row)"  size="large"></el-checkbox>
+        </template>
+       </el-table-column>
+
+        <el-table-column label="코드" width="180">
+            <template #default="scope">
+                <div @click="rowClick(scope)" style="cursor:pointer;">
+                    {{scope.$index+1}}
+                </div>
+            </template>
+        </el-table-column>    
 
         <el-table-column prop="name" label="물품명" width="180" />
         <el-table-column prop="price" label="가격" />
@@ -130,22 +140,54 @@ export default {
     };
   },
   methods: {
-      handleClose(){
-          console.log('Seller => handleClose')
-          this.dialogVisible = false
-      },
-      handleImage(e){
-          console.log(e)
-        //   벡엔드로 변경할 이미지 실제 정보
-          this.itemOne['image1'] = e.target.files[0];
+    async deleteSomeItem(){
+      const url = '/item/deletebatch';
+      const headers = {"Content-Type" : "application/json"};
+      const body = []
+      for(let i =0; i < this.items.length; i++){
+        if(this.items[i].chk){
+        body.push(this.items[i])
+        }
+      }
+      console.log("Seller.vue => deleteSomeItem()" , body)
+      const response = await this.axios.delete(url, {
+      headers : headers,
+      data    : body
+      });
+      console.log("Seller.vue => deleteSomeItem()" , response);
+      if(response.data.status ===200){
+        await this.handleData();
 
-        //   프론트에서 미리보기용 임시 URL
-          this.itemOne['image1URL'] = URL.createObjectURL(e.target.files[0]);
+        // for(let i =0; i < this.items.length; i++){
+        //   this.items[i].chk = false
+        //   console.log(123)
+        // }
+        
+      }
 
-        // dialog img src를 변경하기
-        //   this.itemOne[image] = URL.createObjectURL(e.target.files[0]);
+    }
+    ,
+    clickChk(row){
+      console.log("Seller.vue => clickChk",row)
+    },
 
-      },
+
+    handleClose(){
+        console.log('Seller => handleClose')
+        this.dialogVisible = false
+    },
+    handleImage(e){
+        console.log(e)
+      //   벡엔드로 변경할 이미지 실제 정보
+        this.itemOne['image1'] = e.target.files[0];
+
+      //   프론트에서 미리보기용 임시 URL
+        this.itemOne['image1URL'] = URL.createObjectURL(e.target.files[0]);
+
+      // dialog img src를 변경하기
+      //   this.itemOne[image] = URL.createObjectURL(e.target.files[0]);
+
+    },
     async editConfirm() {
       this.dialogVisible = false;
       const url = `/item/update?code=${this.itemOne._id}`;
@@ -196,10 +238,14 @@ export default {
       const url = `/item/select?page=${this.page}`;
       const headers = { "Content-Type": "application/json" };
       const response = await this.axios.get(url, { headers: headers });
-      console.log(response);
+      console.log("Seller.vue => handleData()",response);
       if (response.data.status === 200) {
         this.items = response.data.result;
         this.total = response.data.total;
+        for(let i =0; i < this.items.length; i++){
+          this.items[i].chk = false
+        }
+        
       }
       // console.log(response)
     },
